5a299bbde27ea4a1ccb913c6a70ac3860ae0a8f9
Jimin Park <pjm950@gmail.com>
2024-07-20 02:42:29 +0900

    Support for absolute path.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs/promises';
-import * as path from 'node:path';
 import { TextChart } from "./text-chart.js";
+import { resolvePath } from "./util.js";
 
 /** @typedef {{file: string, out: string}} Args */
 
@@ -14,9 +14,9 @@ export class Main {
     }
 
     async convert() {
-        const src = await fs.readFile(path.join(process.cwd(), this.args.file), 'utf-8');
+        const src = await fs.readFile(resolvePath(this.args.file), 'utf-8');
         const chart = TextChart.parse(src);
 
-        await fs.writeFile(path.join(process.cwd(), this.args.out), JSON.stringify(chart), 'utf-8');
+        await fs.writeFile(resolvePath(this.args.out), JSON.stringify(chart), 'utf-8');
     }
 }
--- src/util.js
+++ src/util.js
@@ -1,6 +1,16 @@
+import * as path from 'node:path';
+
 /**
  * @param {string} str 
  */
 export function escapeRegExp(str) {
     return str.replace(/[.*+?^${}()|\[\]\\]/g, '\\$&');
+}
+
+/**
+ * @param {string} p 
+ */
+export function resolvePath(p) {
+    if(path.isAbsolute(p)) return p;
+    return path.join(process.cwd(), p);
 }
